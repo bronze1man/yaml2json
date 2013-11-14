521d688ed52a7d57c5758064c3f5889b026ff7a9
bronze1man <bronze1man@gmail.com>
2013-11-14 11:23:56 +0800

    gofmt

--- main.go
+++ main.go
@@ -1,80 +1,80 @@
 package main
 
-
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"launchpad.net/goyaml"
 	"os"
-	"io/ioutil"
-	"fmt"
-	"errors"
 	"strconv"
 )
-func main(){
-	err:=_main()
-	if err!=nil{
-		fmt.Fprintln(os.Stderr,err)
+
+func main() {
+	err := _main()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(2)
 	}
 	os.Exit(0)
 }
-func _main()error{
+func _main() error {
 	var data interface{}
-	input,err:=ioutil.ReadAll(os.Stdin)
-	if err!=nil{
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
 		return err
 	}
-	err=goyaml.Unmarshal(input,&data)
-	if err!=nil{
+	err = goyaml.Unmarshal(input, &data)
+	if err != nil {
 		return err
 	}
-	data,err=transformData(data)
-	if err!=nil{
+	data, err = transformData(data)
+	if err != nil {
 		return err
 	}
 
-	output,err:=json.Marshal(data)
-	if err!=nil{
+	output, err := json.Marshal(data)
+	if err != nil {
 		return err
 	}
-	_,err=os.Stdout.Write([]byte(output))
+	_, err = os.Stdout.Write([]byte(output))
 	return err
 }
-func transformData(in interface {})(out interface{},err error){
-	switch in.(type){
-	case map[interface {}]interface {}:
-		o:=make(map[string]interface {})
-		for k,v:=range in.(map[interface {}]interface {}){
-			sk:=""
-			switch k.(type){
+func transformData(in interface{}) (out interface{}, err error) {
+	switch in.(type) {
+	case map[interface{}]interface{}:
+		o := make(map[string]interface{})
+		for k, v := range in.(map[interface{}]interface{}) {
+			sk := ""
+			switch k.(type) {
 			case string:
-				sk=k.(string)
+				sk = k.(string)
 			case int:
-			    sk=strconv.Itoa(k.(int))
+				sk = strconv.Itoa(k.(int))
 			default:
-				return nil,errors.New(
-				fmt.Sprintf("type not match: expect map key string or int get: %T",k))
+				return nil, errors.New(
+					fmt.Sprintf("type not match: expect map key string or int get: %T", k))
 			}
-			v,err= transformData(v)
-			if err!=nil{
-				return nil,err
+			v, err = transformData(v)
+			if err != nil {
+				return nil, err
 			}
 			o[sk] = v
 		}
-		return o,nil
-	case []interface {}:
-		in1:=in.([]interface {})
-		len1:=len(in1)
-		o:=make([]interface {},len1)
-		for i:=0;i<len1;i++{
-			o[i],err = transformData(in1[i])
-			if err!=nil{
-				return nil,err
+		return o, nil
+	case []interface{}:
+		in1 := in.([]interface{})
+		len1 := len(in1)
+		o := make([]interface{}, len1)
+		for i := 0; i < len1; i++ {
+			o[i], err = transformData(in1[i])
+			if err != nil {
+				return nil, err
 			}
 		}
-		return o,nil
+		return o, nil
 	default:
-		return in,nil
+		return in, nil
 	}
-	return in,nil
+	return in, nil
 }
